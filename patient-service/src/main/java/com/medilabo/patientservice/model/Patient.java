fc0cad2983d9dcfa9ca78395181268e3c3e2f3cc
WebisBrian <brian.t.github@gmail.com>
2026-06-24 16:11:22 +0200

    feat(patient-service): add Patient entity, Gender enum and repository

---
+++ patient-service/src/main/java/com/medilabo/patientservice/model/Patient.java
@@ -0,0 +1,39 @@
+package com.medilabo.patientservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    private String address;
+
+    private String phone;
+}
---
+++ patient-service/src/main/java/com/medilabo/patientservice/repository/PatientRepository.java
@@ -0,0 +1,7 @@
+package com.medilabo.patientservice.repository;
+
+import com.medilabo.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
